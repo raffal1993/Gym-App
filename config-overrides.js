84179ea07c1c58988f1cc73523aac2468fa1f509
raffal1993 <raffal1993@gmail.com>
2022-03-13 20:36:23 +0100

    configs + react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+/* config-overrides.js */
+/* eslint-disable react-hooks/rules-of-hooks */
+const { useBabelRc, override } = require('customize-cra');
+
+module.exports = {
+  webpack: override(useBabelRc()),
+};
