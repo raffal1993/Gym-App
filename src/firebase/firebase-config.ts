f82cb2a3b17d10e44dd62a1867c175ef09c7a9ce
raffal1993 <raffal1993@gmail.com>
2022-03-18 17:09:54 +0100

    install and setup firebase

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBV1a56HT-DkAZORhWRgcxwkSrpP16PsA',
+  authDomain: 'gym-app-89ccd.firebaseapp.com',
+  projectId: 'gym-app-89ccd',
+  storageBucket: 'gym-app-89ccd.appspot.com',
+  messagingSenderId: '933348038402',
+  appId: '1:933348038402:web:eab6a7ef8ac3d4dbd4541b',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
