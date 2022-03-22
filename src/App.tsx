68e810d6ef08c982a13d609ae7d60dc6fb096693
raffal1993 <raffal1993@gmail.com>
2022-03-22 19:49:37 +0100

    add onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import { Wrapper } from 'App.styled';
-import Dashboard from 'components/Dashboard/Dashboard';
-import LoginPanel from 'components/LoginPanel/LoginPanel';
-import PageNotFound from 'components/PageNotFound/PageNotFound';
+import Dashboard from 'components/Pages/Dashboard/Dashboard';
+import PageNotFound from 'components/Organisms/PageNotFound/PageNotFound';
+import { auth } from 'firebase-cfg/firebase-config';
+import { onAuthStateChanged } from 'firebase/auth';
 import AppProviders from 'providers/AppProviders';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import LoginPanel from 'components/Pages/LoginPanel/LoginPanel';
 
 function App() {
+  const [isFirstCheck, setIsFirstCheck] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //   CHECK USER LOGGED LISTENER
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user && window.location.pathname !== '/dashboard') {
+        if (isFirstCheck) {
+          navigate('/dashboard');
+        } else
+          setTimeout(() => {
+            navigate('/dashboard');
+          }, 1500);
+      }
+      if (isFirstCheck) setIsFirstCheck(false);
+    });
+
+    return () => unsubscribe();
+  }, [isFirstCheck, navigate]);
+
   return (
     <AppProviders>
       <Wrapper>
-        <Routes>
-          <Route path="/*" element={<LoginPanel />} />
-          <Route path="/dashboard/*" element={<Dashboard />} />
-          <Route path="/page-not-found" element={<PageNotFound />} />
-        </Routes>
+        {!isFirstCheck && (
+          <Routes>
+            <Route path="/*" element={<LoginPanel />} />
+            <Route path="/dashboard/*" element={<Dashboard />} />
+            <Route path="/page-not-found" element={<PageNotFound />} />
+          </Routes>
+        )}
       </Wrapper>
     </AppProviders>
   );
