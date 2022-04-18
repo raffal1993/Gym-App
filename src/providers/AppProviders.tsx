d326d310ecad0e20b6f88beb4c26adb9cf9b23a9
raffal1993 <raffal1993@gmail.com>
2022-04-18 19:44:33 +0200

    rmv DataProvider

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,20 +1,17 @@
 import { CssBaseline, GlobalStyles } from '@mui/material';
 import { FC, ReactNode } from 'react';
 import globalStyle from 'style/globalStyles';
-import DataProvider from './DataProvider';
 import ReduxProvider from './ReduxProvider';
 import ThemeProvider from './ThemeProvider';
 
 const AppProviders: FC<ReactNode> = ({ children }) => {
   return (
     <ReduxProvider>
-      <DataProvider>
-        <ThemeProvider>
-          <CssBaseline />
-          <GlobalStyles styles={globalStyle} />
-          {children}
-        </ThemeProvider>
-      </DataProvider>
+      <ThemeProvider>
+        <CssBaseline />
+        <GlobalStyles styles={globalStyle} />
+        {children}
+      </ThemeProvider>
     </ReduxProvider>
   );
 };
