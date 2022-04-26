91c14328a47b7548d49dd5ad101b1174ac4ef4d1
raffal1993 <raffal1993@gmail.com>
2022-04-26 13:47:11 +0200

    create component

---
+++ src/components/Organisms/Food/Food.tsx
@@ -0,0 +1,7 @@
+import FoodContent from 'components/Templates/DashboardContent/DashboardContent';
+
+const Food = () => {
+  return <FoodContent>s</FoodContent>;
+};
+
+export default Food;
---
+++ src/components/Organisms/Weather/Weather.tsx
@@ -0,0 +1,7 @@
+import WeatherContent from 'components/Templates/DashboardContent/DashboardContent';
+
+const Weather = () => {
+  return <WeatherContent>s</WeatherContent>;
+};
+
+export default Weather;
