f801757b3a327b02f4b30350930f246dc9bae6f6
raffal1993 <raffal1993@gmail.com>
2022-04-26 13:48:33 +0200

    implement integration with LS + addVersionToDB

--- src/components/Molecules/WorkoutCard/WorkoutCard.tsx
+++ src/components/Molecules/WorkoutCard/WorkoutCard.tsx
@@ -1,27 +1,53 @@
-import VersionButton from 'components/Atoms/VersionButton/VersionButton';
+import VersionButton from 'components/Atoms/Buttons/VersionButton/VersionButton';
 import { importImages } from 'helpers/importImages';
 import { FC, SyntheticEvent, useState } from 'react';
 import { v4 as uuid4 } from 'uuid';
+import { getLocalStorage, updateLocalStorage } from 'helpers/localStorage';
+import { addVersionToDB } from 'firebase-cfg/database';
+import { useAppSelector } from 'app/hooks';
+import AddToDbButton from 'components/Atoms/Buttons/AddToDbButton/AddToDbButton';
+import { RootState } from 'app/store';
 import WorkoutStats from '../WorkoutStats/WorkoutStats';
 import { TitleStyled, VersionsStyled, Wrapper } from './WorkoutCard.styled';
 import { WorkoutCardProps } from '../../Organisms/Workout/WorkoutProps';
 
 const { images } = importImages();
 
-const WorkoutCard: FC<WorkoutCardProps> = ({ name, type, versions }) => {
-  const [selectedVersion, setSelectedVersion] = useState<number>(1);
+const MAX_VERSIONS = 4;
+
+const WorkoutCard: FC<WorkoutCardProps> = ({ exerciseID, name, type, versions }) => {
+  const [selectedVersion, setSelectedVersion] = useState<number>(
+    getLocalStorage('selectedVersions', exerciseID) || 1,
+  );
+
+  const {
+    pages: { subPageID },
+    interface: { isAddModeOn },
+  } = useAppSelector((state: RootState) => state);
 
   const handleVersion = (_event: SyntheticEvent, versionNumber: number) => {
     setSelectedVersion(versionNumber);
+    updateLocalStorage('selectedVersions', { [exerciseID]: versionNumber });
+  };
+
+  const handleAddVersion = () => {
+    if (subPageID && versions.length < MAX_VERSIONS)
+      addVersionToDB(exerciseID, subPageID, versions.length);
   };
 
   return (
     <>
       <Wrapper key={uuid4()} url={images[type]}>
         <TitleStyled>{name.toUpperCase()}</TitleStyled>
-        <WorkoutStats stats={versions[selectedVersion - 1]} />
+
+        <WorkoutStats
+          stats={versions[selectedVersion - 1]}
+          exerciseID={exerciseID}
+          selectedVersion={selectedVersion}
+        />
+
         <VersionsStyled>
-          {versions.map((version, index) => (
+          {versions.map((_version, index) => (
             <VersionButton
               key={uuid4()}
               onClick={(_e: SyntheticEvent) => handleVersion(_e, index + 1)}
@@ -29,6 +55,9 @@ const WorkoutCard: FC<WorkoutCardProps> = ({ name, type, versions }) => {
               versionNumber={index + 1}
             />
           ))}
+          {isAddModeOn && versions.length < MAX_VERSIONS && (
+            <AddToDbButton onClick={handleAddVersion} />
+          )}
         </VersionsStyled>
       </Wrapper>
     </>
