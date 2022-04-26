222cec701d159f4edf14c995250b9561e2c094c3
raffal1993 <raffal1993@gmail.com>
2022-04-26 13:46:33 +0200

    change user check on app start

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,49 @@ import Dashboard from 'components/Pages/Dashboard/Dashboard';
 import { Route, useNavigate } from 'react-router-dom';
 import LoginPanel from 'components/Pages/LoginPanel/LoginPanel';
 import CustomizedRoutes from 'components/Templates/CustomizedRoutes/CustomizedRoutes';
-import { useAppDispatch } from 'app/hooks';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 import PageNotFound from 'components/Organisms/PageNotFound/PageNotFound';
 import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from 'firebase-cfg/firebase-config';
 import { setUser } from 'app/slices/userSlice';
+import { RootState } from 'app/store';
 
 function App() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const { email } = useAppSelector((state: RootState) => state.user);
+
   useEffect(() => {
+    if (email === auth.currentUser?.email) return;
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const userEmail = user.email || '';
         dispatch(setUser({ email: userEmail }));
         if (!!window.location.pathname.match(/^(\/|\/register)$/))
-          setTimeout(() => {
-            navigate('/dashboard');
-          }, 1500);
-      }
+          if (email === null) return navigate('/dashboard');
+          else
+            setTimeout(() => {
+              navigate('/dashboard');
+            }, 1500);
+      } else dispatch(setUser({ email: '' }));
     });
 
-    return () => unsubscribe();
-  }, [dispatch, navigate]);
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch, navigate, email]);
+
   return (
     <Wrapper>
-      <CustomizedRoutes>
-        <Route path="/*" element={<LoginPanel />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-        <Route path="/page-not-found" element={<PageNotFound />} />
-      </CustomizedRoutes>
+      {email !== null && (
+        <CustomizedRoutes>
+          <Route path="/*" element={<LoginPanel />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/page-not-found" element={<PageNotFound />} />
+        </CustomizedRoutes>
+      )}
     </Wrapper>
   );
 }
