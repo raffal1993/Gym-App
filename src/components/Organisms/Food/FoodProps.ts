4584e5f9179be422d18536e4785beb2a87547ed4
raffal1993 <raffal1993@gmail.com>
2022-05-15 23:36:48 +0200

    move types to one file

--- src/components/Organisms/Food/FoodProps.ts
+++ src/components/Organisms/Food/FoodProps.ts
@@ -1,4 +1,4 @@
-interface Nutrients {
+export interface NutrientsAPI {
   ENERC_KCAL?: number;
   FAT?: number;
   CHOCDF?: number;
@@ -6,8 +6,33 @@ interface Nutrients {
   FIBTG?: number;
 }
 
+export interface Column {
+  id: 'name' | 'kcal' | 'fat' | 'carbs' | 'protein' | 'fiber';
+  label: string;
+  minWidth?: number;
+  align?: 'right' | 'left' | 'center';
+}
+
 export interface SearchResultProps {
   label: string;
   image?: string;
-  nutrients: Nutrients;
+  nutrients: NutrientsAPI;
+}
+
+export interface NutrientsDB {
+  id: string;
+  timestamp: number;
+  name: string;
+  kcal: string;
+  fat: string;
+  carbs: string;
+  protein: string;
+  fiber: string;
+}
+
+export interface FoodCardDB {
+  foodCardID: string;
+  timestamp: number;
+  name: string;
+  foodSet: NutrientsDB[];
 }
