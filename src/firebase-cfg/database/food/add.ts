02ea6997c81902bbba943fb0a7407c73cae90dda
raffal1993 <raffal1993@gmail.com>
2022-05-22 00:38:05 +0200

    add: database food functions

---
+++ src/firebase-cfg/database/food/add.ts
@@ -0,0 +1,80 @@
+import {
+  ConvertTimestampDB,
+  FoodCardDB,
+  NutrientsDB,
+  NutrientsTypes,
+} from 'components/Organisms/Food/FoodProps';
+import { getDataFromDB } from 'firebase-cfg/dbHelpers';
+import { child, ref, push, serverTimestamp, update } from 'firebase/database';
+import { auth, db } from '../../firebase-config';
+
+const addFoodSetToDB = (subPageID: string, name: string) => {
+  const uid = auth.currentUser?.uid;
+
+  if (uid) {
+    const targetPath = `users/${uid}/food/${subPageID}`;
+
+    const newFoodSetKey = push(child(ref(db), targetPath)).key;
+    if (!newFoodSetKey) return console.error('newFoodSet is null');
+
+    const newFoodSet: Omit<ConvertTimestampDB<FoodCardDB>, 'foodCardID' | 'foodSet'> = {
+      name,
+      timestamp: serverTimestamp(),
+    };
+
+    const updates = {} as {
+      [key: string]: typeof newFoodSet;
+    };
+
+    updates[targetPath + `/${newFoodSetKey}`] = newFoodSet;
+
+    return update(ref(db), updates);
+  }
+};
+
+const addCustomFoodToDB = async (
+  subPageID: string,
+  name: string,
+  foodCardID: string,
+  nutrients: NutrientsTypes,
+) => {
+  const uid = auth.currentUser?.uid;
+
+  if (uid) {
+    const targetPath = `users/${uid}/food/${subPageID}/${foodCardID}`;
+
+    const data = await getDataFromDB(targetPath)
+      .then((res) => res as Partial<FoodCardDB>)
+      .catch((err) => console.log(err));
+
+    if (data) {
+      let pathToNewFood: string;
+
+      if (data.foodSet === undefined) {
+        pathToNewFood = `${targetPath}/foodSet/0`;
+      } else {
+        pathToNewFood = `${targetPath}/foodSet/${(data.foodSet as NutrientsDB[]).length}`;
+      }
+
+      const newCustomFoodKey = push(child(ref(db), pathToNewFood)).key;
+      if (!newCustomFoodKey) return console.error('newCustomFoodKey is null');
+
+      const newCustomFood: ConvertTimestampDB<NutrientsDB> = {
+        name,
+        timestamp: serverTimestamp(),
+        id: newCustomFoodKey,
+        ...nutrients,
+      };
+
+      const updates = {} as {
+        [key: string]: typeof newCustomFood;
+      };
+
+      updates[pathToNewFood] = newCustomFood;
+
+      return update(ref(db), updates);
+    }
+  }
+};
+
+export { addFoodSetToDB, addCustomFoodToDB };
