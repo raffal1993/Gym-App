22a32642e8453de0769ec74748435f9f35144937
raffal1993 <raffal1993@gmail.com>
2022-05-29 18:49:38 +0200

    implement: pagination

--- src/components/Molecules/SearchFood/SearchFood.tsx
+++ src/components/Molecules/SearchFood/SearchFood.tsx
@@ -1,7 +1,9 @@
 import { FoodApiInstance } from 'api/FoodAPI/instance';
 import Button from 'components/Atoms/Buttons/CustomButton/CustomButton';
 import { setURL } from 'helpers/setURL';
-import React, { FC, useEffect, useRef, useState } from 'react';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import React, { ChangeEvent, FC, useEffect, useRef, useState } from 'react';
 import {
   FoodCardDB,
   NutrientsTypes,
@@ -11,16 +13,18 @@ import {
 import SearchIcon from '@mui/icons-material/Search';
 import ErrorMessage from 'components/Atoms/ErrorMessage/ErrorMessage';
 import Spinner from 'components/Atoms/Spinner/Spinner';
-import { Wrapper, SearchBarStyled } from './SearchFood.styled';
+import { Wrapper, SearchBarStyled, PaginationStyled } from './SearchFood.styled';
 import SearchResult from '../SearchResults/SearchResults';
 
-const INPUTS = {
-  search: 'search',
-  page: 'page',
-};
+type SearchFoodMethod = 'searchByPageNumber' | 'searchByPhrase';
+interface SearchFoodProps {
+  handleScrollTop: () => void;
+  foodCards: FoodCardDB[];
+}
 
-const SearchFood: FC<{ foodCards: FoodCardDB[] }> = ({ foodCards }) => {
+const SearchFood: FC<SearchFoodProps> = ({ foodCards, handleScrollTop }) => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [searchingPhrase, setSearchingPhrase] = useState<string>('');
   const [pageNumber, setPageNumber] = useState<number>(1);
   const [searchResults, setSearchResults] = useState<SearchFoodItemTypes[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -28,21 +32,45 @@ const SearchFood: FC<{ foodCards: FoodCardDB[] }> = ({ foodCards }) => {
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     if (e.target.getAttribute('data-type') === INPUTS.search) setInputValue(e.target.value);
-    //     if (e.target.getAttribute('data-type')
-    // === INPUTS.page) setPageNumber(Number(e.target.value));
-
-    //     if (inputRef.current) inputRef.current.value = e.target.value;
-
+  const handleInputSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
-  const handleSearchFood = async () => {
+  const handleInputPageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let { value } = e.currentTarget;
+    value = value.toString();
+
+    let newValue = value[0] === '0' && value.length > 1 ? value.slice(1) : value;
+    newValue = Number(newValue) < 0 ? '1' : newValue;
+
+    setPageNumber(Number(newValue));
+  };
+
+  const handleSearchFood = async (searchType: SearchFoodMethod, page: number = 1) => {
     setErrorMessage('');
-    if (inputValue) {
+    let validation = false;
+    let searchingValue = '';
+
+    if (searchType === 'searchByPhrase' && inputValue) {
+      setSearchingPhrase(inputValue);
+      setPageNumber(1);
+      validation = true;
+      searchingValue = inputValue;
+    }
+
+    if (searchType === 'searchByPageNumber' && searchingPhrase) {
+      page === 0 ? setPageNumber(1) : setPageNumber(page);
+      validation = true;
+      searchingValue = searchingPhrase;
+      handleScrollTop();
+      setSearchResults([]);
+    }
+
+    //     validation = false;
+
+    if (validation) {
       setIsLoading(true);
-      await FoodApiInstance.get(`${setURL(inputValue, pageNumber)}`)
+      await FoodApiInstance.get(`${setURL(searchingValue, page)}`)
         .then((res) => {
           const foods = res.data.hints.map(
             ({ food: { image, label, nutrients } }: { food: FoodDB }) => ({
@@ -61,7 +89,10 @@ const SearchFood: FC<{ foodCards: FoodCardDB[] }> = ({ foodCards }) => {
 
           setSearchResults(foods);
         })
-        .catch(() => setErrorMessage('An error has occurred'))
+        .catch(() => {
+          setErrorMessage('An error has occurred');
+          setPageNumber(1);
+        })
         .finally(() => setIsLoading(false));
     }
   };
@@ -81,21 +112,53 @@ const SearchFood: FC<{ foodCards: FoodCardDB[] }> = ({ foodCards }) => {
           <SearchIcon />
           <input
             ref={inputRef}
-            onKeyPress={(e) => e.key === 'Enter' && handleSearchFood()}
+            onKeyPress={(e) => e.key === 'Enter' && handleSearchFood('searchByPhrase')}
             placeholder="(english only)"
             type="text"
-            data-type={INPUTS.search}
-            onChange={(e) => onInputChange(e)}
+            onChange={(e) => handleInputSearchChange(e)}
             value={inputValue}
           />
         </label>
-        <Button handleClick={handleSearchFood}>Search Food</Button>
+        <Button handleClick={() => handleSearchFood('searchByPhrase')}>Search Food</Button>
         {isLoading && <Spinner />}
         {errorMessage && <ErrorMessage className="errorMessage" errorMessage={errorMessage} />}
         <p className="info">All nutrition information is provided per 100 grams of product</p>
       </SearchBarStyled>
       {!!searchResults.length && (
         <SearchResult foodCards={foodCards} searchResults={searchResults} />
+      )}
+      {searchResults.length !== 0 && (
+        <PaginationStyled>
+          <Button
+            disabled={pageNumber <= 1}
+            className="button prevButton"
+            handleClick={() => handleSearchFood('searchByPageNumber', Number(pageNumber) - 1)}
+          >
+            <ArrowBackIosNewIcon />
+            prev page
+          </Button>
+
+          <div className="goToPage">
+            <input
+              type="number"
+              value={pageNumber.toString()}
+              onChange={(e) => handleInputPageChange(e)}
+            />
+            <Button
+              className="button pageButton"
+              handleClick={() => handleSearchFood('searchByPageNumber', Number(pageNumber))}
+            >
+              <ArrowForwardIosIcon />
+            </Button>
+          </div>
+          <Button
+            className="button nextButton"
+            handleClick={() => handleSearchFood('searchByPageNumber', Number(pageNumber) + 1)}
+          >
+            next page
+            <ArrowForwardIosIcon />
+          </Button>
+        </PaginationStyled>
       )}
     </Wrapper>
   );
