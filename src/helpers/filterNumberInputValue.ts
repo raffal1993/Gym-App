5adae38a283928ba6422525a6785b80577cf817c
raffal1993 <raffal1993@gmail.com>
2022-05-29 19:50:54 +0200

    fix: input filter fn

--- src/helpers/filterNumberInputValue.ts
+++ src/helpers/filterNumberInputValue.ts
@@ -2,6 +2,7 @@ export const filterNumberInputValue = (value: string) => {
   value = value[0] === '.' ? `0${value}` : value;
   value = value.slice(0, 2) === '00' ? `0${value.slice(2)}` : value;
   value = value.length > 1 && value[1] !== '.' && value[0] === '0' ? value.slice(1) : value;
+  value = Number(value) < 0 ? '0' : value;
 
   return value;
 };
