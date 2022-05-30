f2664ae9cb5007dc254c672deacfb1cdcbdd2863
raffal1993 <raffal1993@gmail.com>
2022-05-30 11:17:45 +0200

    add: className prop, change: styles

--- src/components/Atoms/Spinner/Spinner.tsx
+++ src/components/Atoms/Spinner/Spinner.tsx
@@ -1,12 +1,21 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import { FC } from 'react';
 
-const Spinner = () => {
+interface SpinnerProps {
+  className?: string;
+}
+
+const Spinner: FC<SpinnerProps> = ({ className }) => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
+    <Box className={className} sx={{ display: 'flex' }}>
+      <CircularProgress style={{ width: '100%', height: '100%' }} />
     </Box>
   );
 };
 
 export default Spinner;
+
+Spinner.defaultProps = {
+  className: '',
+};
