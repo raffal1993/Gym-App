7f399015f06adef039ede41c72b68545d7c9d510
raffal1993 <raffal1993@gmail.com>
2022-05-31 11:38:16 +0200

    implement: env variables

--- src/firebase-cfg/firebase-config.ts
+++ src/firebase-cfg/firebase-config.ts
@@ -3,13 +3,13 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDBV1a56HT-DkAZORhWRgcxwkSrpP16PsA',
-  authDomain: 'gym-app-89ccd.firebaseapp.com',
-  projectId: 'gym-app-89ccd',
-  storageBucket: 'gym-app-89ccd.appspot.com',
-  messagingSenderId: '933348038402',
-  appId: '1:933348038402:web:eab6a7ef8ac3d4dbd4541b',
-  databaseURL: 'https://gym-app-89ccd-default-rtdb.europe-west1.firebasedatabase.app',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
