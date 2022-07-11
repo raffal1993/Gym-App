9dd9a0ad3a48c0f17c6bb7bdb0cf52f4ef0b003b
raffal1993 <raffal1993@gmail.com>
2022-07-11 08:12:18 +0200

    change: redux config files for testing

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, PreloadedState } from '@reduxjs/toolkit';
 import interfaceReducer from './slices/interfaceSlice';
 import userReducer from './slices/userSlice';
 import pagesReducer from './slices/pagesSlice';
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    interface: interfaceReducer,
-    pages: pagesReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['interface/setModalOpen'],
-        ignoredPaths: ['interface.modalContent'],
-      },
-    }),
+export const rootReducer = combineReducers({
+  user: userReducer,
+  interface: interfaceReducer,
+  pages: pagesReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export type AppStore = typeof store;
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: ['interface/setModalOpen'],
+          ignoredPaths: ['interface.modalContent'],
+        },
+      }),
+  });
+}
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
