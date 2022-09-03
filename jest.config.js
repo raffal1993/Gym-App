a0ed501cee5845f0f0663b57a55b541e3ac10949
raffal1993 <raffal1993@gmail.com>
2022-09-03 15:49:00 +0200

    add: setup mocks

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,13 @@ module.exports = {
       isolatedModules: true,
     },
   },
+
+  setupFiles: [
+    '<rootDir>/src/__tests__/setupMocks/auth.ts',
+    '<rootDir>/src/__tests__/setupMocks/uuid.ts',
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleDirectories: ['node_modules', 'src'],
   roots: ['<rootDir>/src/__tests__'],
   testMatch: ['**/*.(test|spec).(tsx|ts|jsx|js)'],
-  modulePathIgnorePatterns: ['<rootDir>/src/__tests__/done', '<rootDir>/node_modules'],
 };
