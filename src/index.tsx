ea73275fb7af2ac924ab8bdd3fea7dc5c997d3c7
raffal1993 <raffal1993@gmail.com>
2022-09-06 18:39:55 +0200

    remove providers folder, move to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import 'style/fonts.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'App';
-import AppProviders from 'providers/AppProviders';
+import { Provider } from 'react-redux';
+import { setupStore } from 'app/store';
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
+import theme from 'style/theme';
+import globalStyle from 'style/globalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProviders>
-        <App />
-      </AppProviders>
+      <Provider store={setupStore()}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <GlobalStyles styles={globalStyle} />
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/providers/ReduxProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { Provider } from 'react-redux';
-import { FC, ReactNode } from 'react';
-import { setupStore } from 'app/store';
-
-const ReduxProvider: FC<ReactNode> = ({ children }) => {
-  return <Provider store={setupStore()}>{children}</Provider>;
-};
-
-export default ReduxProvider;
