e1dbb93f64283d833d7256ff5f0e434e77b43e7d
raffal1993 <raffal1993@gmail.com>
2022-09-07 20:37:23 +0200

    refactor: testing utils

--- src/__tests__/utils/globalTheme-utils.tsx
+++ src/__tests__/utils/globalTheme-utils.tsx
@@ -1,11 +1,11 @@
-import { CssBaseline, GlobalStyles } from '@mui/material';
-import ThemeProvider from 'providers/ThemeProvider';
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
 import { FC, ReactNode } from 'react';
 import globalStyle from 'style/globalStyles';
+import theme from 'style/theme';
 
 const GlobalThemeProvider: FC<ReactNode> = ({ children }) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <GlobalStyles styles={globalStyle} />
       {children}
